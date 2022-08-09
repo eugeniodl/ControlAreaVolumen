dccf9be3c18b5520e9f87418fd4ea8985f77cca3
Eugenio Díaz <Eugenio.Diaz@fcys.uni.edu.ni>
2022-08-09 16:40:41 -0600

    GUI

--- ControlAreaVolumen/frmControlAreaVolumen.Designer.cs
+++ ControlAreaVolumen/frmControlAreaVolumen.Designer.cs
@@ -45,9 +45,10 @@
             // 
             this.pictureBox1.BackgroundImage = global::ControlAreaVolumen.Properties.Resources.banner_3;
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(112, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(128, 3);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(609, 145);
+            this.pictureBox1.Size = new System.Drawing.Size(696, 193);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -55,49 +56,52 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(250, 150);
+            this.label1.Location = new System.Drawing.Point(286, 200);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(319, 30);
+            this.label1.Size = new System.Drawing.Size(401, 37);
             this.label1.TabIndex = 1;
             this.label1.Text = "CONTROL DE ÁREA Y VOLUMEN";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(112, 205);
+            this.label2.Location = new System.Drawing.Point(128, 273);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(49, 15);
+            this.label2.Size = new System.Drawing.Size(62, 20);
             this.label2.TabIndex = 2;
             this.label2.Text = "ALTURA";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(259, 205);
+            this.label3.Location = new System.Drawing.Point(296, 273);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(65, 15);
+            this.label3.Size = new System.Drawing.Size(83, 20);
             this.label3.TabIndex = 3;
             this.label3.Text = "DIÁMETRO";
             // 
             // txtAltura
             // 
-            this.txtAltura.Location = new System.Drawing.Point(112, 243);
+            this.txtAltura.Location = new System.Drawing.Point(128, 324);
+            this.txtAltura.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtAltura.Name = "txtAltura";
-            this.txtAltura.Size = new System.Drawing.Size(100, 23);
+            this.txtAltura.Size = new System.Drawing.Size(114, 27);
             this.txtAltura.TabIndex = 4;
             // 
             // txtDiametro
             // 
-            this.txtDiametro.Location = new System.Drawing.Point(259, 243);
+            this.txtDiametro.Location = new System.Drawing.Point(296, 324);
+            this.txtDiametro.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtDiametro.Name = "txtDiametro";
-            this.txtDiametro.Size = new System.Drawing.Size(100, 23);
+            this.txtDiametro.Size = new System.Drawing.Size(114, 27);
             this.txtDiametro.TabIndex = 5;
             // 
             // btnCalcular
             // 
-            this.btnCalcular.Location = new System.Drawing.Point(259, 285);
+            this.btnCalcular.Location = new System.Drawing.Point(296, 380);
+            this.btnCalcular.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnCalcular.Name = "btnCalcular";
-            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.Size = new System.Drawing.Size(114, 31);
             this.btnCalcular.TabIndex = 6;
             this.btnCalcular.Text = "CALCULAR";
             this.btnCalcular.UseVisualStyleBackColor = true;
@@ -105,35 +109,38 @@
             // 
             // txtR
             // 
-            this.txtR.Location = new System.Drawing.Point(415, 205);
+            this.txtR.Location = new System.Drawing.Point(474, 273);
+            this.txtR.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtR.Multiline = true;
             this.txtR.Name = "txtR";
-            this.txtR.Size = new System.Drawing.Size(306, 171);
+            this.txtR.Size = new System.Drawing.Size(349, 227);
             this.txtR.TabIndex = 7;
             // 
             // btnLimpiar
             // 
-            this.btnLimpiar.Location = new System.Drawing.Point(545, 394);
+            this.btnLimpiar.Location = new System.Drawing.Point(575, 525);
+            this.btnLimpiar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnLimpiar.Name = "btnLimpiar";
-            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.Size = new System.Drawing.Size(103, 31);
             this.btnLimpiar.TabIndex = 8;
             this.btnLimpiar.Text = "LIMPIAR";
             this.btnLimpiar.UseVisualStyleBackColor = true;
             // 
             // btnSalir
             // 
-            this.btnSalir.Location = new System.Drawing.Point(646, 394);
+            this.btnSalir.Location = new System.Drawing.Point(705, 525);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.Size = new System.Drawing.Size(119, 31);
             this.btnSalir.TabIndex = 9;
             this.btnSalir.Text = "SALIR";
             this.btnSalir.UseVisualStyleBackColor = true;
             // 
             // frmControlAreaVolumen
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(914, 600);
             this.Controls.Add(this.btnSalir);
             this.Controls.Add(this.btnLimpiar);
             this.Controls.Add(this.txtR);
@@ -144,6 +151,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "frmControlAreaVolumen";
             this.Text = "Control de área y volumen";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
